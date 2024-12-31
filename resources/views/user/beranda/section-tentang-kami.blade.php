2e44a323de43f13f3c98077a29407d88e9a61865
StringyMeatloaf <damanikrayakha@gmail.com>
2024-12-31 16:06:49 +0700

    tambah gambar

--- resources/views/user/beranda/section-tentang-kami.blade.php
+++ resources/views/user/beranda/section-tentang-kami.blade.php
@@ -1,18 +1,27 @@
-<div>
-    <div class="flex items-center gap-[8px] mb-[2px]">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#008ED6" class="bi bi-building-fill" viewBox="0 0 16 16">
+<div class="grid grid-cols-2 gap-4">
+    <div>
+        <div class="flex items-center gap-[8px] mb-[2px]">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#008ED6" class="bi bi-building-fill" viewBox="0 0 16 16">
             <path d="M3 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h3v-3.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V16h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm1 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5M4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5m2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5"/>
-        </svg>
-        <h6 class="text-base font-semibold text-[#008ED6]">Tentang Kami
-        </h6>
+            </svg>
+            <h6 class="text-base font-semibold text-[#008ED6]">Tentang Kami
+            </h6>
+        
+        </div>
+        <h1 class="text-[40px] font-bold">Klinik Care<span class="text-[#008ED6]">Link</span></h1>
+        <h1 class="text-[18px] text-gray-500 leading-[27px] mb-[20px]">
+        Selamat datang di Klinik CareLink, mitra terpercaya Anda dalam perawatan kecantikan dan kesehatan kulit. Kami berdedikasi untuk memberikan layanan terbaik dengan teknologi modern, produk berkualitas, dan tim profesional yang berpengalaman.
+        </h1>
+        <a  
+        href="/tentang-kami" class="px-6 py-2 bg-[#008ED6] text-white rounded-md hover:bg-blue-600 transition]">
+        Selengkapnya <i class="bi bi-arrow-right ml-[4px]"></i>
+        </a>
     </div>
-    <h1 class="text-[40px] font-bold">Klinik Care<span class="text-[#008ED6]">Link</span></h1>
-    <h1 class="text-[18px] text-gray-500 leading-[27px] mb-[20px]">
-    Selamat datang di Klinik CareLink, mitra terpercaya Anda dalam perawatan kecantikan dan kesehatan kulit. Kami berdedikasi untuk memberikan layanan terbaik dengan teknologi modern, produk berkualitas, dan tim profesional yang berpengalaman.
-    </h1>
-    <a href="/tentang-kami" class="px-6 py-2 bg-[#008ED6] text-white rounded-md hover:bg-blue-600 transition">
-    Selengkapnya <i class="bi bi-arrow-right ml-[4px]"></i>
-</a>
+    <div>
+        <img src="{{ asset(path: 'img/dokter.jpg') }}" alt="logo">
+    </div>
+</div>
+    
 
 <span class="text-[#008ED6]"></span>
 </h1>
