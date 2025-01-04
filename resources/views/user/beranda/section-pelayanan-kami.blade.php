4b2f4b5036a90506bed0d16dde15ac6b9196e002
aNopall <ahmad.noval2705@gmail.com>
2025-01-04 21:53:07 +0700

    refactor layout and styling for service section on homepage

--- resources/views/user/beranda/section-pelayanan-kami.blade.php
+++ resources/views/user/beranda/section-pelayanan-kami.blade.php
@@ -1,68 +1,68 @@
-<div class="flex flex-col items-center">
+<div class="flex flex-col items-center max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="flex items-center gap-[8px] mb-[2px]">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#008ED6" class="bi bi-headset" viewBox="0 0 16 16">
             <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5"/>
         </svg>
         <h6 class="text-base font-semibold text-[#008ED6]">Pelayanan Kami</h6>
     </div>
-    <h1 class="text-[40px] font-bold mb-[50px]">Solusi kesehatan Anda, setiap saat</h1>
-</div>
-<div class="container mx-auto px-6">
-  
-  <div class="grid grid-cols-2 gap-6">
-    <!-- Card 1 -->
-  <div>
-    <div class="flex gap-4">
-      <div class="w-[60px] h-[60px] bg-gray-300 rounded">
-        <!-- Tempat gambar -->
-      </div>
-      <div>
-        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
-        <p class="text-sm text-gray-600 mb-[20px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-        </p>
-      </div>
-    </div>
+    <h1 class="text-[40px] font-bold mb-6">Solusi kesehatan Anda, setiap saat</h1>
+    <div>
+        <div class="grid grid-cols-2 gap-6">
+            <!-- Card 1 -->
+            <div>
+                <div class="flex gap-4">
+                    <div class="w-[60px] h-[60px] bg-gray-300 rounded">
+                        <!-- Tempat gambar -->
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
+                        <p class="text-sm text-gray-600 mb-[20px]">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                        </p>
+                    </div>
+                </div>
 
-    <!-- Card 2 -->
-    <div class="flex gap-4">
-      <div class="w-[60px] h-[60px] bg-gray-300 rounded">
-        <!-- Tempat gambar -->
-      </div>
-      <div>
-        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
-        <p class="text-sm text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-        </p>
-      </div>
-    </div>
-  </div>
-  <div>
-    <!-- Card 3 -->
-    <div class="flex gap-4">
-      <div class="w-[60px] h-[60px] bg-gray-300 rounded">
-        <!-- Tempat gambar -->
-      </div>
-      <div>
-        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
-        <p class="text-sm text-gray-600 mb-[20px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-        </p>
-      </div>
-    </div>
+                <!-- Card 2 -->
+                <div class="flex gap-4">
+                    <div class="w-[60px] h-[60px] bg-gray-300 rounded">
+                        <!-- Tempat gambar -->
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
+                        <p class="text-sm text-gray-600">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                        </p>
+                    </div>
+                </div>
+            </div>
 
-    <!-- Card 4 -->
-    <div class="flex gap-4">
-      <div class="w-[60px] h-[60px] bg-gray-300 rounded">
-        <!-- Tempat gambar -->
-      </div>
-      <div>
-        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
-        <p class="text-sm text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-        </p>
-      </div>
+            <div>
+                <!-- Card 3 -->
+                <div class="flex gap-4">
+                    <div class="w-[60px] h-[60px] bg-gray-300 rounded">
+                        <!-- Tempat gambar -->
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
+                        <p class="text-sm text-gray-600 mb-[20px]">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Card 4 -->
+                <div class="flex gap-4">
+                    <div class="w-[60px] h-[60px] bg-gray-300 rounded">
+                        <!-- Tempat gambar -->
+                    </div>
+                    <div>
+                        <h3 class="text-lg font-bold">Lorem Ipsum Dolor Sit Amet</h3>
+                        <p class="text-sm text-gray-600">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </div>
