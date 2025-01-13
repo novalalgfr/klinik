e79ef9b22dc40b0b4e874fbd729fb5effd91ac88
Rhapsodyss <alvarizky200@gmail.com>
2025-01-13 22:01:32 +0700

    juna-ubah card

--- resources/views/user/blog/blog.blade.php
+++ resources/views/user/blog/blog.blade.php
@@ -11,23 +11,28 @@
     </div>
     <div class="w-full flex-shrink-0">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-12">
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-            @foreach ($blogs as $blog)
-            <!-- Card -->
-                <div class="bg-white shadow-md rounded-lg overflow-hidden p-4 h-[450px] border">
-                    <div class="">
-                        <img src="{{ asset('storage/' . $blog->gambar) }}" alt="" class="h-[128px] w-[286px] rounded">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                @foreach ($blogs as $blog)
+                    <!-- Card -->
+                    <div class="bg-white shadow-md rounded-lg overflow-hidden p-4 border flex flex-col justify-between w-full h-[450px]">
+                        <!-- Image -->
+                        <div class="flex justify-center">
+                            <img src="{{ asset('storage/' . $blog->gambar) }}" alt="" class="h-[128px] w-[286px] object-cover rounded">
+                        </div>
+                        <!-- Content -->
+                        <div class="flex flex-col justify-between flex-1 mt-4">
+                            <div>
+                                <p class="text-sm text-[#008ED6]">{{ $blog->created_at->translatedFormat('d F Y') }}</p>
+                                <h2 class="mt-2 text-base font-semibold text-gray-800">{{ $blog->judul }}</h2>
+                                <p class="mt-2 text-sm text-gray-600 text-left line-clamp-4 ">{{ $blog->deskripsi_pendek }}</p>
+                            </div>
+                            <!-- Button -->
+                            <a href="#" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 text-[13px] self-start">
+                                Selengkapnya →
+                            </a>
+                        </div>
                     </div>
-                    <div class="p-4">
-                        <p class="text-sm text-[#008ED6]">{{ $blog->created_at->translatedFormat('d F Y') }}</p>
-                        <h2 class="mt-2 text-base font-semibold text-gray-800">{{ $blog->judul }}</h2>
-                        <p class="mt-2 text-sm text-gray-600 text-justify h-[150px]">{{ $blog->deskripsi_pendek }}</p>
-                        <a href="#" class="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 text-[13px]">
-                        Selengkapnya →
-                        </a>
-                    </div>
-                </div>
-            @endforeach    
+                @endforeach
             </div>
         </div>
     </div>
